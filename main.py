5789a1a29cad8e33833bfa5701faf826791942a4
madsschroder <Machelle2021!>
2022-03-06 23:10:10 +0100

    First iteration design + unusable code

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QDialog, QApplication
+from PyQt6.uic import loadUi
+
+class Login(QDialog):
+    def __init__(self):
+        super(Login,self).__init__()
+        loadUi("Login.ui",self)
+        self.LoginButton.click.connect(self.loginfunction)
+
+    def loginfunction(self):
+        Unilogin=self.Unilogin.text()
+        Password=self.Password.text()
+        print("Godkendt login, Bruger: ", Unilogin, "og adgangskode: ", Password)
+
+
+app=QApplication(sys.argv)
+mainwindow=Login()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(420)
+widget.setFixedHeight(520)
+widget.show()
+app.exec_()
