9f5fa669e00bae0c12462888b4df848b8fbd860c
madsschroder <Machelle2021!>
2022-04-19 20:17:28 +0200

    Login ui opdatering

--- test.py
+++ test.py
@@ -5,9 +5,29 @@ from PyQt6 import uic
 class LoginUI(QMainWindow):
     def __init__(self):
         super(LoginUI, self).__init__()
+
+        #Loader den specifikke UI fil
         uic.loadUi("Login.ui",self)
+
+        #Definerer UI filens widgets
+        self.Unilogin = self.findChild(QLineEdit, "Unilogin")
+        self.Password = self.findChild(QLineEdit, "Password")
+        self.LoginButton = self.findChild(QPushButton, "LoginButton")
+        self.GlemtLoginButton = self.findChild(QPushButton, "GlemtLoginButton")
+
+        #Hvad widgets skal gøre
+        self.LoginButton.clicked.connect(self.loginfunction)
+
+        #Viser appen
         self.show()
 
+    def loginfunction(self):
+        unilogin=self.Unilogin.text()
+        password=self.Password.text()
+        print("Du er logget ind med email:", unilogin, "og password:", password)
+
+
+#Initialiserer appen
 app = QApplication(sys.argv)
 UIWindow = LoginUI()
 app.exec()
