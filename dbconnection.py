e45936bf00f814facd241c6f6a258dc225a8de4a
schrodm2 <mads.schroder@roche.com>
2022-04-20 20:37:34 +0200

    databas3

--- dbconnection.py
+++ dbconnection.py
@@ -1,16 +1,34 @@
 import mysql.connector
+from mysql.connector import Error
 
-mydb = mysql.connector.connect(
-    host='uniskemadb-do-user-11280721-0.b.db.ondigitalocean.com',
-    port=25060,
-    database='defaultdb',
-    user='doadmin',
-    password='AVNS_qt-pIDU320VjzTt')
+def get_connection():
+    connection = mysql.connector.connect(
+        host='uniskemadb-do-user-11280721-0.b.db.ondigitalocean.com',
+        port=25060,
+        database='defaultdb',
+        user='doadmin',
+        password='AVNS_qt-pIDU320VjzTt')
+    return connection
 
+def close_connection(connection):
+    if connection:
+        connection.close()
 
-mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM users")
-sqladmin = mycursor.fetchall()
+def get_users(users):
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        select_query = """select * from defaultdb.users where id =%s"""
+        cursor.execute(select_query, (users,))
+        records = cursor.fetchall()
+        print("Udskriver brugeren med")
+        for row in records:
+            print("id:", row[0], )
+            print("navn:", row[1])
+            print("kode:", row[2])
+            print("lavet:", row[3])
+        close_connection(connection)
+    except (Exception, mysql.connector.Error) as error:
+        print("Error while getting data", error)
 
-def doQuery()
-print(sqladmin)
+get_users(1)
