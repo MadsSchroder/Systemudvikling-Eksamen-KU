bc2b1914017533274638e410bab82e8dd06a3326
schrodm2 <mads.schroder@roche.com>
2022-04-24 13:18:56 +0200

    Teacher opdatering

--- dbconnection.py
+++ dbconnection.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 from datetime import date
-
+from instanceclasses import instantiateclasses
 
 def get_connection():
     connection = mysql.connector.connect(
@@ -78,7 +78,77 @@ def get_classes_on_date(userid):
     connection.close()
     return class_list
 
+# def get_classes_on_datess():
+#     connection = get_connection()
+#     mycursor = connection.cursor()
+#     query = ("SELECT * FROM classes")
+#     mycursor.execute(query)
+#     class_list = []
+#     result = mycursor.fetchall()
+#     for row in result:
+#         class_list.append(row)
+#     mycursor.close()
+#     connection.close()
+#     return class_list
 
-test=check_password("Student", "test")
-print(test)
+def testere():
+    connection = get_connection()
+    mycursor = connection.cursor()
+    new = testlist = []
+    query = ("SELECT * FROM classes")
+    mycursor.execute(query)
+    classfetch = mycursor.fetchall()
+    for (classeslist) in range(len(classfetch)):
+        testlist.append(
+            instantiateclasses(classfetch[classeslist][0], classfetch[classeslist][1], classfetch[classeslist][2],
+                    classfetch[classeslist][3], classfetch[classeslist][4], classfetch[classeslist][5]))
 
+    return new
+
+def CREATE_NEW_CLASSES(location, date, start, end, id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    maininput = (location, date, start, end, id)
+    query = """INSERT INTO classes(location, classdate, classstart, classend, courseid) VALUES (%s, %s, %s, %s, %s)"""
+    cursor.execute(query, maininput)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def UPDATE_LOCATION(location, id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    maininput = (location, id)
+    query = """UPDATE classes SET location = (%s) WHERE id = (%s)"""
+    cursor.execute(query, maininput,)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def UPDATE_DATE(date, id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    maininput = (date, id)
+    query = """UPDATE classes SET classdate = (%s) WHERE id = (%s)"""
+    cursor.execute(query, maininput,)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def UPDATE_TIME(start, end, id):
+    connection = get_connection()
+    cursor = connection.cursor()
+    maininput = (start, end, id)
+    query = """UPDATE classes SET classstart = (%s), classend = (%s) WHERE id = (%s)"""
+    cursor.execute(query, maininput,)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+#CREATE_NEW_CLASSES('Programmering - fra python','2022-04-30','08:00:00','17:00:00', 1)
+#UPDATE_LOCATION('Python', 11)
+#UPDATE_DATE('2022-04-01', 11)
+#UPDATE_TIME('08:00:00', '13:00:00', 11)
+#get_classes_on_datess()
+#print(get_classes_on_datess)
---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+from PyQt6.QtWidgets import QMainWindow, QApplication, QLabel, QLineEdit, QPushButton
+from PyQt6 import uic
+import dbconnection
+import Teacher
+
+class Login(QMainWindow):
+    def __init__(self):
+        super(Login, self).__init__()
+
+        #Loader den specifikke UI fil
+        uic.loadUi("UI/Login.ui",self)
+
+        #Definerer UI filens widgets
+        self.Unilogin = self.findChild(QLineEdit, "Unilogin")
+        self.Password = self.findChild(QLineEdit, "Password")
+        self.LoginButton = self.findChild(QPushButton, "LoginButton")
+        self.GlemtLoginButton = self.findChild(QPushButton, "GlemtLoginButton")
+
+        #Hvad widgets skal gøre
+        self.LoginButton.clicked.connect(self.loginfunction)
+
+        #Viser appen
+        self.show()
+
+    def loginfunction(self):
+        unilogin = self.Unilogin.text()
+        password = self.Password.text()
+        verification = dbconnection.check_password(unilogin, password)
+        if verification[0] == True and verification[2] == 1:
+            uic.loadUi("UI/studentwindow.ui", self)
+            print(verification)
+        elif verification[0] == True and verification[2] == 2:
+            print(verification)
+            uic.loadUi("UI/teacherwindow.ui", self)
+            Teacher.calendarDateChanged()
+
+        elif verification[0] == True and verification[2] == 3:
+            uic.loadUi("UI/adminwindow.ui", self)
+            print(verification)
+        else:
+            print("error", verification)
+
+
+#Initialiserer appen
+app = QApplication(sys.argv)
+UIWindow = Login()
+UIWindow.show()
+app.exec()
